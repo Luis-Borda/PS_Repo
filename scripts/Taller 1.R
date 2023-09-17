ca9ce94d39a7d1ca16648b37f61e41e2932991f9
Luis-Borda <142829063+Luis-Borda@users.noreply.github.com>
2023-09-16 19:47:00 -0500

    Update Taller 1.R

--- scripts/Taller 1.R
+++ scripts/Taller 1.R
@@ -2,6 +2,36 @@
 ######## PUNTO 1 #######
 #### CARGUE DE BASE ####
 ## Creamos un vector con los diferentes links ##
+
+#Limpieza del área de trabajo 
+
+rm(list = ls())
+
+#CARGAMOS LOS PAQUETES
+
+
+library(pacman)
+
+p_load(rio,
+       tidyverse,
+       skimr,
+       caret,
+       readxl,
+       rvest,
+       stargazer,
+       knitr,
+       boot,
+       data.table)
+
+library(dplyr)
+library(tidyr)
+
+#CREAMOS UN DIRECTORIO
+
+setwd("/Users/lordb/OneDrive - Universidad de los andes/2. GRUPOS/2. BIG DATA/PS_Repo_Taller1_G10/scripts")
+
+#Hacemos el scraping
+
 link <- paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
 
 
@@ -24,15 +54,10 @@ vacios$porcentaje_vacios <- round(vacios$apply.X...is.na.base...MARGIN...2..FUN.
 colnames(vacios)[1]<- "Cantidad_vacios"
 
 ## Se seleccionan las varaibles de interés
-base2 <- select(base, "estrato1",
-                "sex",
-                "age",
-                "p6240",
-                "p6426",
-                "ina",
-                "maxEducLevel",
-                "ocu",
-                "dsi")
+base2 <- base %>%
+  select(directorio,estrato1,sex,age,p6240,p6426,ina,maxEducLevel,ocu,dsi,
+         y_total_m,y_total_m_ha,ingtot,ingtotob,microEmpresa,cuentaPropia,formal, 
+         oficio,p6210,p7090,hoursWorkUsual,relab,sizeFirm)
 
 ## Se filtra la base ##
 base_filtrada <- subset(base2, base2$age >= 18 & base2$ocu == 1)
