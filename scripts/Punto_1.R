3a1d82b995f4bc0843b2dfd56eaef686d0a27337
jspotosi <joansp1004@gmail.com>
2023-09-19 01:59:12 -0500

    cambios

--- scripts/Punto_1.R
+++ scripts/Punto_1.R
@@ -86,23 +86,7 @@ write.xlsx(descriptivas, "stores/Descriptivas.xlsx")
 
 ##### Gráficos para interpretación
 
-# 1. Crear el histograma
-hist(b_final$estrato1)
-
-hist(base_filtrada$y_total_m_ha)
-hist(base_filtrada$maxEducLevel)
-hist(base_filtrada$p6240)
-
-#Se defninen las variables categoricas
-
-Categoricas <- c("estrato1","relab","sizeFirm","maxEducLevel","p6240")
-
-# Iterar a través de las columnas en el vector Categoricas
-for (v in Categoricas) {
-  # Seleccionar la columna v del dataframe b_final y convertirla en factor
-  b_final[, v] <- as.factor(b_final[, v, drop = TRUE])
-}
-
+# Histograma edad
 
 edad <- ggplot(data = base_filtrada,
                mapping = aes(x = age))  + 
@@ -120,6 +104,7 @@ edad <- ggplot(data = base_filtrada,
 
 edad
 
+##Histograma ingreso
 
 Ingreso <- ggplot(data = base_filtrada,
                   mapping = aes(x = y_total_m_ha))  + 
@@ -134,43 +119,10 @@ Ingreso <- ggplot(data = base_filtrada,
        x = 'Ingreso',
        y = 'Frecuencia') + 
   theme_bw()
+
 Ingreso
 
-histogram(b_final$y_total_m_ha)
-
-# Crear un gráfico de dispersión con una curva de suavizado en color
-scatter.smooth(b_final$age, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Edad", ylab = "Ingreso",
-               evaluation = 50, col = "blue") 
-title("Relación entre Edad e Ingreso")
-
-# Crear un gráfico de dispersión con una curva de suavizado en color
-scatter.smooth(b_final$estrato1, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Estrato", ylab = "Ingreso",
-               evaluation = 50, col = "green")  
-title("Relación entre Estrato e Ingreso")
-
-# Crear un gráfico de cajas y bigotes con estrato1 en el eje X y y_total_m_ha en el eje Y
-boxplot(b_final$y_total_m_ha ~ b_final$estrato1, 
-        xlab = "Estrato", ylab = "Ingreso",
-        main = "Ingreso vs Estrato")
-
-# Crear un gráfico de cajas y bigotes con relab en el eje X y y_total_m_ha en el eje Y
-boxplot(b_final$y_total_m_ha ~ b_final$relab, 
-        xlab = "Tipo de ocupación", ylab = "Ingreso",
-        main = "Ingreso vs Tipo de ocupación")
-
-
-scatter.smooth(b_final$hoursWorkUsual, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Horas trabajadas", ylab = "Ingreso",
-               evaluation = 50, col = "green")  
-title("Ingreso vs horas trabajadas")
-
-
-### Primero se hace un plot del ingreso para ver su distribución
+### relación ingreso edad
 
 ggplot(data=base_filtrada)+ 
   geom_smooth(mapping = aes(x =age, y =y_total_m_ha))+
@@ -182,8 +134,22 @@ ggplot(data=base_filtrada)+
   scale_y_continuous (labels=function(n) {format(n, scientific = FALSE)})+
   labs(x="Edad", y="Ingreso")
 
- +
-  
+### relación ingreso estrato
+
+#Grfico de dispesión con línea de tendencia
+scatter.smooth(base_filtrada$estrato1, base_filtrada$y_total_m_ha, span = 2/3, degree = 1,
+               family = "symmetric",
+               xlab = "Estrato", ylab = "Ingreso",
+               evaluation = 50, col = "green")  
+title("Relación entre Estrato e Ingreso")
+
+# gráfico de cajas y bigotes
+boxplot(base_filtrada$y_total_m_ha ~ base_filtrada$estrato1, 
+        xlab = "Estrato", ylab = "Ingreso",
+        main = "Ingreso vs Estrato")
+
+
+##### relación experiencia ingreso ################################
   
 ggplot(data=base_filtrada)+ 
   geom_smooth(mapping = aes(x =p6426, y =y_total_m_ha))+
@@ -194,146 +160,3 @@ ggplot(data=base_filtrada)+
   geom_point(mapping = aes(x =p6426, y =y_total_m_ha))+
   scale_y_continuous (labels=function(n) {format(n, scientific = FALSE)})+
   labs(x="Experiencia", y="Ingreso")
-
-+
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## Se dispone a eliminar todos los vacios
-
-nombres <- colnames(base)
-write.csv(nombres,"Nombres varaibles.csv")
-
-#eliminamos las observaciones con NA de nuestra variable de interes, no se imputa
-#Nos queda una observación con NA en maxEducLevel, la eliminamos
-b_final <- base_filtrada %>% filter(y_total_m_ha != "NA")
-
-b_final <- b_final %>% filter(maxEducLevel != "NA")
-data.frame(apply(X = is.na(b_final), MARGIN =2, FUN = sum))
-summary(base_final)
-
-b_final <- b_final %>% 
-  mutate(age_2 = age^2)
-
-b_final <- b_final %>%
-  mutate(log_Ingresos = log(y_total_m_ha))
-
-b_final$female <- ifelse(b_final$sex == 0, 1, 0) %>% as.numeric()
-
-
-write.csv(b_final,file = "stores/b_final.csv",row.names = FALSE)
-
-#read_csv("stores/b_final.csv")
-#View(b_final)
-
-################################################################################
-##ESTADISTICAS DESCRIPTIVAS 
-################################################################################
-library(stargazer)
-library(knitr)
-Descrip <- b_final %>%
-  select(directorio,estrato1,sex,age,p6240,p6426,maxEducLevel,ocu,dsi,
-         y_total_m,y_total_m_ha,ingtot,ingtotob,microEmpresa,cuentaPropia,formal, 
-         oficio,p6210,p7090,hoursWorkUsual,relab,sizeFirm)
-
-Descriptivas <- summary(Descrip)
-tabla_descriptiva <-kable(Descriptivas, caption = "Estadísticas descriptivas")
-
-write.csv(tabla_descriptiva, "stores/tabla_descriptiva.csv")
-
-
-# 1. Crear el histograma
-hist(b_final$estrato1)
-
-hist(b_final$relab)
-hist(b_final$sizeFirm)
-hist(b_final$maxEducLevel)
-hist(b_final$p6240)
-
-#Se defninen las variables categoricas
-
-Categoricas <- c("estrato1","relab","sizeFirm","maxEducLevel","p6240")
-
-# Iterar a través de las columnas en el vector Categoricas
-for (v in Categoricas) {
-  # Seleccionar la columna v del dataframe b_final y convertirla en factor
-  b_final[, v] <- as.factor(b_final[, v, drop = TRUE])
-}
-
-
-edad <- ggplot(data = b_final,
-               mapping = aes(x = age))  + 
-  geom_histogram(aes(y =after_stat(density)),
-                 bins = 5,
-                 position = 'identity',
-                 color="black", fill="gray" ) +
-  stat_function(fun = dnorm, xlim = c(min(b_final$age),max(b_final$age)), colour="green", linewidth=1,
-                args = list(mean = mean(b_final$age), 
-                            sd = sd(b_final$age))) + 
-  labs(title = 'Distribución de edad',
-       x = 'Edad',
-       y = 'Frecuencia') + 
-  theme_bw()
-
-edad
-
-
-Ingreso <- ggplot(data = b_final,
-                  mapping = aes(x = y_total_m_ha))  + 
-  geom_histogram(aes(y =after_stat(density)),
-                 bins = 5,
-                 position = 'identity',
-                 color="black", fill="gray" ) +
-  stat_function(fun = dnorm, xlim = c(min(b_final$y_total_m_ha),max(b_final$y_total_m_ha)), colour="green", linewidth=1,
-                args = list(mean = mean(b_final$y_total_m_ha), 
-                            sd = sd(b_final$y_total_m_ha))) + 
-  labs(title = 'Distribución del ingreso',
-       x = 'Edad',
-       y = 'Frecuencia') + 
-  theme_bw()
-Ingreso
-
-histogram(b_final$y_total_m_ha)
-
-# Crear un gráfico de dispersión con una curva de suavizado en color
-scatter.smooth(b_final$age, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Edad", ylab = "Ingreso",
-               evaluation = 50, col = "blue") 
-title("Relación entre Edad e Ingreso")
-
-# Crear un gráfico de dispersión con una curva de suavizado en color
-scatter.smooth(b_final$estrato1, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Estrato", ylab = "Ingreso",
-               evaluation = 50, col = "green")  
-title("Relación entre Estrato e Ingreso")
-
-# Crear un gráfico de cajas y bigotes con estrato1 en el eje X y y_total_m_ha en el eje Y
-boxplot(b_final$y_total_m_ha ~ b_final$estrato1, 
-        xlab = "Estrato", ylab = "Ingreso",
-        main = "Ingreso vs Estrato")
-
-# Crear un gráfico de cajas y bigotes con relab en el eje X y y_total_m_ha en el eje Y
-boxplot(b_final$y_total_m_ha ~ b_final$relab, 
-        xlab = "Tipo de ocupación", ylab = "Ingreso",
-        main = "Ingreso vs Tipo de ocupación")
-
-
-scatter.smooth(b_final$hoursWorkUsual, b_final$y_total_m_ha, span = 2/3, degree = 1,
-               family = "symmetric",
-               xlab = "Horas trabajadas", ylab = "Ingreso",
-               evaluation = 50, col = "green")  
-title("Ingreso vs horas trabajadas")
